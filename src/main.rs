a264d7bbbda4ed9fa15a968396e2844d4f0bd6e6
Prokhorov Egor <prokhorov.egor9@gmail.com>
2025-10-22 09:08:09 +0300

    ch

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,59 @@ fn main()
     };
     println!("{}", qwer);
     //loops();
+    for i in 1..11
+    {
+        for j in 1..11
+        {
+            print!("{}\t", i * j);
+        }
+        println!();
+        
+    }
+    // square(5);
+    // square(10);
+    // square(2);
+    // square(99);
+    let check: i32 = chec();
+    println!("{}", check);
+
+    //анонимные функции 
+    let square_anon = |n: i32|{
+        let result = n * n;
+        println!("Квадрат числа {} равен {}", n, result);
+    };
+    square_anon(5);
+
+    let block = {
+        println!("kek");
+        println!("kek");
+        println!("kek");
+        println!("kek");
+        99
+    };
+    println!("block = {block}");
+
+
+
+    let tuple: (&'static str, i32, f64) = ("kek", 1, 1.5);
+    println!("tuple 0: {}", tuple.0);
+    let mut kek: &'static str = tuple.0;
+    print!("{}", kek);
+    kek = "keks";
+
+    let num:[i32; 3] = [1, 2, 1];
+    for i in 0..num.len()
+    {
+        println!("{}", num[i]);
+    }
+
+
+    let mut users = ["Tom", "Bob", "Sam"];
+    users.sort();
+    println!("users: {:?} ", users);
 }
+
+
 
 fn summ(){
     let mut x: i32 = 5;
@@ -41,18 +93,46 @@ fn matchh(age:f32){
     };
     println!("{}", result);
 }
+
+fn square(n: i32)
+{
+    let result: i32 = n * n;
+    println!("Квадрат числа {} равен {}", n, result);
+}
+
+fn chec() -> i32
+{
+    5
+}
 fn loops(){
     let mut days: i32 = 0;
     let mut check: bool = false;
-    loop {
+    loop 
+    {
         days = days + 1;
-        if days == 100 && check == false{
+        if days == 100 && check == false
+        {
             days = 0;
             check = true;
         }
-        else if days == 100 && check == true{
+        else if days == 100 && check == true
+        {
             break;
         }
-        println!("days = {}", days);
+        println!("while days = {}", days);
     }
+    days = 0;
+    while days < 100
+    {
+        days = days + 1;
+        println!("for days = {}", days);
+    }
+
+    for i in 0..10
+    {
+        println!("i = {}", i);
+    }
+
+
+
 }
